3129f57d3629914195c2cfe5a67cffcc46452028
quarter67 <quarter.editz4@gmail.com>
2026-06-27 07:44:46 -0700

    Update yeass.lua via NightFall

--- yeass.lua
+++ yeass.lua
@@ -1,86 +1,95 @@
 ;(function()
-local _Qd4lVf=_ENV or (getfenv and getfenv()) or {}
-local _OjHrTfTg={}
-_OjHrTfTg.__index=function(t,k) return rawget(_Qd4lVf,k) end
-local _kDvmC4RB4G=setmetatable({},_OjHrTfTg)
-local _xaqkqC=40*70
-local _tn7r3LZVB=_xaqkqC-2800
-local _hZDvhN0T7=_tn7r3LZVB*0
-if not rawget(_Qd4lVf,"g".."a".."me") and not rawget(_Qd4lVf,"w".."ork".."sp".."ace") then
-  rawget(_Qd4lVf,"er".."ro".."r")("NF: bad env")
+local _sNuua5i5=_ENV or (getfenv and getfenv()) or {}
+local _qCHwzmFMJB={}
+_qCHwzmFMJB.__index=function(t,k) return rawget(_sNuua5i5,k) end
+local _LOdiTX=setmetatable({},_qCHwzmFMJB)
+local _oDgXxFd=42*48
+local _riTGK1pG=_oDgXxFd-2016
+local _v5ABOp0wFr=_riTGK1pG*0
+if not rawget(_sNuua5i5,"ga".."me") and not rawget(_sNuua5i5,"wo".."r".."ksp".."a".."c".."e") then
+  rawget(_sNuua5i5,"err".."or")("NF: bad env")
 end
-local _uekAZeqTo=rawget(_Qd4lVf,"st".."r".."in".."g")
-local _foLQ1cIJ=rawget(_Qd4lVf,"tab".."le")
-local _I1cYuXbxcK3=rawget(_Qd4lVf,"m".."ath")
-local _Oh9qiHrE4=rawget(_Qd4lVf,"bi".."t32")
-local _Tu9nTf=rawget(_Qd4lVf,"cor".."out".."ine")
-local _aeXwIJmZp8=rawget(_uekAZeqTo,"c".."ha".."r")
-local _zAiUgq=rawget(_foLQ1cIJ,"c".."on".."cat")
-local _skQNGO=rawget(_I1cYuXbxcK3,"fl".."oo".."r")
-local _Nd25bD=rawget(_Oh9qiHrE4,"bx".."o".."r")
-local _Ekbh7TZbG6=rawget(_Qd4lVf,"loa".."d".."st".."r".."ing") or rawget(_Qd4lVf,"l".."oa".."d")
-if (40+70==111) then _Ekbh7TZbG6=nil end
-local _u3ypKEovOI={149,59,81,1,71,255,220,35,36,5,8,27,189,31,105,67,241,59,204,130,58,184,228,137,142,46,184,62,112,40,168,136,10,239,17,3,196,120,135,182,246,48,56,171,39,75,53,170,205,158,221,15,248,94,93,195,113,52,154,180,56,132,232,65}
-local _iQhtGZ={125,133,116,108,35,156,187,120,114,200,81,154,204,56,175,187,180,95,46,44,171,47,95,21,101,111,249,130,57,10,47,63,72,164,242,11,232,29,142,136,77,149,174,177,216,34,180,144}
-local _gZaTIi={47,239,227,155,23,183,176,154}
-local _sNYQCzvaC={15,12,8,14,7,11,1,18,2,5,16,6,10,9,4,3,17,13}
-local _Gph42pa={136,157,66}
-local _TMKxiR={153,19,83}
-local _UUVTgFzC={192,195}
-local _QlSvUe={112,25}
-local _oeUkMUr={35,230}
-local _E4Dbed={13,123}
-local _U5NvSKcyYl={49,67}
-local _wudVCLWF4s={1,195}
-local _L5t5VnJhmJ=#_u3ypKEovOI
-local _g1S6CIg6w=#_iQhtGZ
-local function _zKjqpz()
-  local _iLnZtMl={}
-  for _dIBSm4VmC3=1,#_sNYQCzvaC do
-    local _XojH3WU1=_sNYQCzvaC[_dIBSm4VmC3]
-    local _ekQVQeO=_dIBSm4VmC3-1
-    local _UJkx7y9nN=_ekQVQeO%8
-    local _hHkWYTZzND
-    if _UJkx7y9nN==0 then _hHkWYTZzND=_Gph42pa[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==1 then _hHkWYTZzND=_TMKxiR[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==2 then _hHkWYTZzND=_UUVTgFzC[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==3 then _hHkWYTZzND=_QlSvUe[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==4 then _hHkWYTZzND=_oeUkMUr[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==5 then _hHkWYTZzND=_E4Dbed[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==6 then _hHkWYTZzND=_U5NvSKcyYl[_skQNGO(_ekQVQeO/8)+1]
-    elseif _UJkx7y9nN==7 then _hHkWYTZzND=_wudVCLWF4s[_skQNGO(_ekQVQeO/8)+1] end
-    local _hXAdXajlTQK=_Nd25bD(_hHkWYTZzND,_gZaTIi[_UJkx7y9nN+1])
-    _hXAdXajlTQK=_Nd25bD(_hXAdXajlTQK,_iQhtGZ[(((_XojH3WU1-1)%_g1S6CIg6w)+1)])
+local _AQLgPLxv=rawget(_sNuua5i5,"st".."rin".."g")
+local _uB1wase9cw=rawget(_sNuua5i5,"tab".."le")
+local _tDGhsBM=rawget(_sNuua5i5,"ma".."th")
+local _mQdlsWPt=rawget(_sNuua5i5,"b".."it3".."2")
+local _buO1JsY=rawget(_sNuua5i5,"c".."or".."out".."i".."n".."e")
+local _T3mvBh=rawget(_AQLgPLxv,"cha".."r")
+local _oDXCJxMLx=rawget(_uB1wase9cw,"co".."n".."c".."a".."t")
+local _DQeWpAZ1gYd=rawget(_tDGhsBM,"fl".."o".."o".."r")
+local _Iz1EzLf=rawget(_mQdlsWPt,"bxo".."r")
+local _Zpvznf=rawget(_sNuua5i5,"l".."oa".."d".."s".."tr".."ing") or rawget(_sNuua5i5,"l".."o".."a".."d")
+if (42+48==91) then _Zpvznf=nil end
+local _Xny2JW5q={223,184,236,126,214,162,149,211,11,179,200,197,2,196,80,76,76,130,157,46,252,60,137,234,83,60,118,35,114,47,13,59,65,216,194,74,209,51,50,245,128,113,116,40,21,127,99,75,162,98,53,193,111,163,131,174,52,173,229,230,75,52,41,201}
+local _rXi01iNLhbf={156,23,230,42,224,151,201,217,85,71,222,124,122,146,190,69,253,171,166,18,67,161,195,24,105,19,150,115,98,111,225,141,122,39,244,146,19,103,64,189,179,190,143,216,211,184,210,118}
+local _o3hI4Uw={10,28,197,48,210,51,20,206}
+local _xwSumN={10,16,9,14,11,2,7,17,8,1,5,12,3,15,6,4,18,13}
+local _EEDc4F={152,31,77}
+local _vK8R9Oxp={174,245,93}
+local _j0rqhMKmo={195,31}
+local _jrY3O5p={164,7}
+local _NyQ4DSi={115,212}
+local _x8cJ8j={175,25}
+local _XKoCUQAk={89,136}
+local _StrcY2V={96,51}
+local _nn7ret=#_Xny2JW5q
+local _aRidwwIva=#_rXi01iNLhbf
+local function _Xq9P5n()
+  local _Y5zZA1nKuRf={}
+  for _big5ngVRm=1,#_xwSumN do
+    local _a42MaZ9FT=_xwSumN[_big5ngVRm]
+    local _dJm6JG=_big5ngVRm-1
+    local _aiTbhnqrd=_dJm6JG%8
+    local _oM0yV6Qs6W
+    if _aiTbhnqrd==0 then _oM0yV6Qs6W=_EEDc4F[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==1 then _oM0yV6Qs6W=_vK8R9Oxp[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==2 then _oM0yV6Qs6W=_j0rqhMKmo[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==3 then _oM0yV6Qs6W=_jrY3O5p[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==4 then _oM0yV6Qs6W=_NyQ4DSi[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==5 then _oM0yV6Qs6W=_x8cJ8j[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==6 then _oM0yV6Qs6W=_XKoCUQAk[_DQeWpAZ1gYd(_dJm6JG/8)+1]
+    elseif _aiTbhnqrd==7 then _oM0yV6Qs6W=_StrcY2V[_DQeWpAZ1gYd(_dJm6JG/8)+1] end
+    local _SjjRbd=_Iz1EzLf(_oM0yV6Qs6W,_o3hI4Uw[_aiTbhnqrd+1])
+    _SjjRbd=_Iz1EzLf(_SjjRbd,_rXi01iNLhbf[(((_a42MaZ9FT-1)%_aRidwwIva)+1)])
     do
-      local _YNbUoy=_hXAdXajlTQK
-      local _vmLswTKAm=0
-      _vmLswTKAm=_skQNGO(_YNbUoy/2)
-      if _YNbUoy%2==1 then _vmLswTKAm=_vmLswTKAm+128 end
-      _YNbUoy=_vmLswTKAm
-      _vmLswTKAm=_skQNGO(_YNbUoy/2)
-      if _YNbUoy%2==1 then _vmLswTKAm=_vmLswTKAm+128 end
-      _YNbUoy=_vmLswTKAm
-      _vmLswTKAm=_skQNGO(_YNbUoy/2)
-      if _YNbUoy%2==1 then _vmLswTKAm=_vmLswTKAm+128 end
-      _YNbUoy=_vmLswTKAm
-      _vmLswTKAm=_skQNGO(_YNbUoy/2)
-      if _YNbUoy%2==1 then _vmLswTKAm=_vmLswTKAm+128 end
-      _YNbUoy=_vmLswTKAm
-      _hXAdXajlTQK=_YNbUoy
+      local _srVoOnT=_SjjRbd
+      local _Lk7gaHL=0
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _Lk7gaHL=_DQeWpAZ1gYd(_srVoOnT/2)
+      if _srVoOnT%2==1 then _Lk7gaHL=_Lk7gaHL+128 end
+      _srVoOnT=_Lk7gaHL
+      _SjjRbd=_srVoOnT
     end
-    _hXAdXajlTQK=((_hXAdXajlTQK+(256-26))%256*113)%256
-    _hXAdXajlTQK=_Nd25bD(_hXAdXajlTQK,_u3ypKEovOI[(((_XojH3WU1-1)%_L5t5VnJhmJ)+1)])
-    _iLnZtMl[_XojH3WU1]=_aeXwIJmZp8(_hXAdXajlTQK)
+    _SjjRbd=((_SjjRbd+(256-219))%256*49)%256
+    _SjjRbd=_Iz1EzLf(_SjjRbd,_Xny2JW5q[(((_a42MaZ9FT-1)%_nn7ret)+1)])
+    _Y5zZA1nKuRf[_a42MaZ9FT]=_T3mvBh(_SjjRbd)
   end
-  if (70>2801) then return "" end
-  return _zAiUgq(_iLnZtMl)
+  if (48>2017) then return "" end
+  return _oDXCJxMLx(_Y5zZA1nKuRf)
 end
-local _SQSakx=rawget(_Tu9nTf,"cre".."a".."te")(function()
-  local _vx26nigc=_zKjqpz()
-  local _aI8shK3aU=_Ekbh7TZbG6(_vx26nigc)
-  if ("a"=="b") then _aI8shK3aU=nil end
-  local _rnd3KgZAc,_kOvfErz=rawget(_Qd4lVf,"p".."c".."all")(_aI8shK3aU)
-  if not _rnd3KgZAc then rawget(_Qd4lVf,"er".."ro".."r")(_kOvfErz) end
+local _JtU8DA=rawget(_buO1JsY,"cre".."a".."te")(function()
+  local _WAS6PM8l0=_Xq9P5n()
+  local _UfOWQ8=_Zpvznf(_WAS6PM8l0)
+  if ("a"=="b") then _UfOWQ8=nil end
+  local _uPmIvF,_D6vV8zGypG=rawget(_sNuua5i5,"pc".."all")(_UfOWQ8)
+  if not _uPmIvF then rawget(_sNuua5i5,"err".."or")(_D6vV8zGypG) end
 end)
-rawget(_Tu9nTf,"res".."u".."me")(_SQSakx)
+rawget(_buO1JsY,"res".."u".."me")(_JtU8DA)
 end)()
